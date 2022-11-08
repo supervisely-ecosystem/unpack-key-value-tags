64823a499e9a59bf53087bca449d26467a447297
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2022-11-08 21:23:48 +0400

    fix input tag values (#5)
    * fix custom project name, remove original tags from dst project meta
    
    * debug modal
    
    * update imports
    
    * log unpacked tags
    
    * add more logs
    
    * add more logs
    
    * add more logs
    
    * remove logs
    
    * remove debug from modal
    
    * remove pycache

--- src/globals.py
+++ src/globals.py
@@ -1,12 +1,12 @@
 import os
 import sys
 
-import supervisely_lib as sly
+import supervisely as sly
 from supervisely.app.v1.app_service import AppService
 
 app_root_directory = os.path.dirname(os.getcwd())
 sys.path.append(app_root_directory)
-sys.path.append(os.path.join(app_root_directory, "unpack-key-value-tags", "src"))
+sys.path.append(os.path.join(app_root_directory, "src"))
 print(f"App root directory: {app_root_directory}")
 sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
 
@@ -16,14 +16,20 @@ sly.logger.info(f'PYTHONPATH={os.environ.get("PYTHONPATH", "")}')
 # load_dotenv(os.path.join(app_root_directory, "unpack-key-value-tags", "debug.env"))
 
 my_app = AppService()
+api = sly.Api.from_env()
 
 TASK_ID = int(os.environ["TASK_ID"])
 TEAM_ID = int(os.environ["context.teamId"])
 WORKSPACE_ID = int(os.environ["context.workspaceId"])
 PROJECT_ID = int(os.environ["modal.state.slyProjectId"])
 
-SELECTED_TAGS = [i.strip() for i in os.environ["modal.state.tags"][1:-1].replace('"',"").split(',')]
+SELECTED_TAGS = [i.strip() for i in os.environ["modal.state.tags"][1:-1].replace('"',"").replace("'", "").split(',')]
+if not SELECTED_TAGS:
+    message = "Please select at least 1 tag"
+    sly.logger.error(msg=message)
+    raise Exception(message)
+
 KEEP_TAGS = os.environ["modal.state.keepTags"]
 INPUT_PROJECT_NAME = str(os.environ["modal.state.inputProjectName"])
 
-api = sly.Api.from_env()
+unpacked_tags_names = []
